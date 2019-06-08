ff3d060ab29e273ce2ea258342705b238c342e6c
zimajp <majimoji@gmail.com>
2019-06-09 03:03:37 +0700

    AdminにModel追加

--- pleethai/admin.py
+++ pleethai/admin.py
@@ -2,18 +2,42 @@ from django.contrib import admin
 
 from import_export import resources
 from import_export.admin import ImportExportModelAdmin
-#from pleethai.models import SampleModel ※要置換
+from pleethai.models import Word, WordClass, Example, Constituent
 
 # TODO 以下は作成したモデルに従って修正する
-# class SampleModelResource(resources.ModelResource):
+class WordResource(resources.ModelResource):
 
-#     class Meta:
-#         model = SampleModel
-#         fields = ('id', 'name', 'sex',)
+    class Meta:
+        model = Word
 
-# @admin.register(SampleModel)
-# class SampleModelAdmin(ImportExportModelAdmin):
-#     resource_class = SampleModelResource
+class WordClassResource(resources.ModelResource):
 
+    class Meta:
+        model = WordClass
 
-    
+class ExampleResource(resources.ModelResource):
+
+    class Meta:
+        model = Example
+
+class ConstituentResource(resources.ModelResource):
+
+    class Meta:
+        model = Constituent
+
+class WordAdmin(ImportExportModelAdmin):
+    resource_class = WordResource
+
+class WordClassAdmin(ImportExportModelAdmin):
+    resource_class = WordClassResource
+
+class ExampleAdmin(ImportExportModelAdmin):
+    resource_class = ExampleResource
+
+class ConstituentAdmin(ImportExportModelAdmin):
+    resource_class = ConstituentResource
+
+admin.site.register(Word, WordAdmin)
+admin.site.register(WordClass,WordClassAdmin)
+admin.site.register(Example, ExampleAdmin)
+admin.site.register(Constituent, ConstituentAdmin)
