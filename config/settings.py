763aef30e0e0d0cac7e6641f167c61c1f8323047
nomrand <42882840+nomrand@users.noreply.github.com>
2019-08-13 23:49:30 +0700

    smtp setting changed (connect gmail SMTP server)

--- config/settings.py
+++ config/settings.py
@@ -160,17 +160,17 @@ SASS_TEMPLATE_EXTS = ['.html', '.haml']
 # FOR DEBUG
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'localhost'
-EMAIL_PORT = 1025
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
-EMAIL_USE_TLS = False
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'test.gaifaa@gmail.com'
+EMAIL_HOST_PASSWORD = 'gaifaa2019'
+EMAIL_USE_TLS = True
 
 REQUSET_MAIL_SEND_INFO = {
     'subject': 'Pleethai Request Recieved',
     'templete_path': 'mails/request.txt',
-    'from_email': 'from@test.com',
+    'from_email': 'test.gaifaa@gmail.com',
     'recipient_list': [
-        'to@test.com',
+        'test.gaifaa@gmail.com',
     ],
 }
