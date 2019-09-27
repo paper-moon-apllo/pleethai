76b71639b620620c481577ce5b24a1e6da2faf5d
zimajp <majimoji@gmail.com>
2019-09-27 15:38:08 +0700

    ページの先頭に戻るボタン追加

--- static/js/search.js
+++ static/js/search.js
@@ -130,6 +130,31 @@ $(document).ready(function(){
         badgeClickedFlag = false;
         searchedFlag = false;
     });
+
+    // Page top button
+    var appear = false;
+    var pagetop = $('#page_top');
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 100) {  //100pxスクロールしたら
+        if (appear == false) {
+          appear = true;
+          pagetop.stop().animate({
+            'bottom': '20px' //下から20pxの位置に
+          }, 300); //0.3秒かけて現れる
+        }
+      } else {
+        if (appear) {
+          appear = false;
+          pagetop.stop().animate({
+            'bottom': '-50px' //下から-50pxの位置に
+          }, 300); //0.3秒かけて隠れる
+        }
+      }
+    });
+    pagetop.click(function () {
+      $('body, html').animate({ scrollTop: 0 }, 500); //0.5秒かけてトップへ戻る
+      return false;
+    });
 });
 
 function initTimer() {
